57da8d30f258cad2a57ceff53ab3559db76cfa9c
vitsensei <tranluuvietanh@gmail.com>
2020-11-23 20:56:06 +1030

    Improve print out for Payment model.

--- PayMyBill/bill_payer/models.py
+++ PayMyBill/bill_payer/models.py
@@ -30,5 +30,5 @@ class Payment(models.Model):
     status = models.IntegerField(default=0)
 
     def __str__(self):
-        return f"{self.name} (${self.amount})"
+        return f"{self.company.name} to {self.name} (${self.amount})"
 
