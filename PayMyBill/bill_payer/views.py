e36d707cebf1000bd6ded1c0220f0e8875d61771
vitsensei <tranluuvietanh@gmail.com>
2020-11-23 21:41:47 +1030

    Add error handling for bill_payer's views.

--- PayMyBill/bill_payer/views.py
+++ PayMyBill/bill_payer/views.py
@@ -1,4 +1,5 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
+from .models import Company, Payment
 
 
 def index(request):
@@ -6,9 +7,30 @@ def index(request):
 
 
 def payments(request, company_name):
-    return HttpResponse(f"Page to mange all the payments of company {company_name}")
+    try:
+        company = Company.objects.get(name=company_name)
+
+    except Company.DoesNotExist:
+        raise Http404(f"Company {company_name} does not exist.")
+
+    else:
+        return HttpResponse(f"Page to mange all the payments of company {company_name}")
 
 
 def details(request, company_name, payment_id):
-    return HttpResponse(f"Details of one payment with id {payment_id} of company {company_name}")
+    try:
+        company = Company.objects.get(name=company_name)
+
+    except Company.DoesNotExist:
+        raise Http404(f"Company {company_name} does not exist.")
+
+    else:
+        try:
+            payment = Company.payment_set.get(name=payment_id)
+
+        except Payment.DoesNotExist:
+            raise Http404(f"Payment {payment_id} of company {company_name} does not exist.")
+
+        else:
+            return HttpResponse(f"Details of one payment with id {payment_id} of company {company_name}")
 
