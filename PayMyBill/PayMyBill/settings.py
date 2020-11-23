b773f959eda90fd46786c528e5ceb62a93e8bf3e
vitsensei <tranluuvietanh@gmail.com>
2020-11-23 20:46:35 +1030

    Add basic models and migrated database.

--- PayMyBill/PayMyBill/settings.py
+++ PayMyBill/PayMyBill/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'bill_payer.apps.BillPayerConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
