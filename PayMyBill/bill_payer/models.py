f3a9488b6bc760a0039b741b46063cc880da6ce8
vitsensei <tranluuvietanh@gmail.com>
2020-11-25 18:13:48 +1030

    changes in models to keep consistency

--- PayMyBill/bill_payer/models.py
+++ PayMyBill/bill_payer/models.py
@@ -22,10 +22,9 @@ class Payment(models.Model):
 
     name = models.CharField(max_length=200)
     bsb = models.CharField(max_length=200)
-    account = models.CharField(max_length=200)
+    account_num = models.CharField(max_length=200)
 
     create_date = models.DateTimeField('publish date')
-    pay_date = models.DateTimeField('pay date')
 
     status = models.IntegerField(default=0)
 
